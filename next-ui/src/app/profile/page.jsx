8c42c15f44a8781baf731e40822184965296c61b
maria-pvp <m.usenko@pvp.co.jp>
2024-09-12 15:01:57 +0900

    Rewrite Profile module to use context

--- next-ui/src/app/profile/page.jsx
+++ next-ui/src/app/profile/page.jsx
@@ -1,12 +1,27 @@
-import GetUserEmail from "../ui/serveractions/GetUserEmail";
-export const dynamicParams = false;
+//Commented out below is alternative method which retrieves user email from cookies, not context.
+//I will leave it here in case we opt for cookies instead of context in the future.
+// import GetUserEmail from "../ui/serveractions/GetUserEmail";
+// export const dynamicParams = false;
 
-export default async function Profile() {
-  const email = await GetUserEmail();
+// export default async function Profile() {
+//   const email = await GetUserEmail();
+//   return (
+//     <div>
+//       <h1>Profile</h1>
+//       {email && <p>You are logged in as {email.value}</p>}
+//     </div>
+//   );
+// }
+
+
+"use client";
+import { LoggedInUserProvider } from "../ui/contexts/LoggedInUserContext";
+import LoggedInUserDetails from "../ui/components/LoggedInUserDetails";
+export default function Profile() {
   return (
-    <div>
+    <LoggedInUserProvider>
       <h1>Profile</h1>
-      {email && <p>You are logged in as {email.value}</p>}
-    </div>
+      <LoggedInUserDetails />
+    </LoggedInUserProvider>
   );
 }
