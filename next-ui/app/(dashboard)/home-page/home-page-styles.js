f0a2126b2fe57beedfa397ec11735bdbfe597f49
maria-pvp <m.usenko@pvp.co.jp>
2024-09-25 17:43:21 +0900

    Merge dummy homepage with the navbar

---
+++ next-ui/app/(dashboard)/home-page/home-page-styles.js
@@ -0,0 +1,4 @@
+export const homePageStyles = {
+  alertChips: 'flex rounded-md text-base font-normal px-4 py-2 bg-white-14',
+  domainInsightsCard: 'min-w-72 min-h-60 rounded-md mb-7 me-7 bg-white-14',
+}
