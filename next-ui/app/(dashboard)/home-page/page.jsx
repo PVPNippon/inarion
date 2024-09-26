1d013c644c1400a8400fc8c618cc807b72735846
maria-pvp <m.usenko@pvp.co.jp>
2024-09-26 09:43:08 +0900

    Add comments

--- next-ui/app/(dashboard)/home-page/page.jsx
+++ next-ui/app/(dashboard)/home-page/page.jsx
@@ -1,5 +1,14 @@
 import '../../ui/globals.css'
 import { homePageStyles } from './home-page-styles'
+
+//at the moment this page is a dummy with some hard-coded data. You can view it by running the ui folder on localhost:3000 or docker and accessing it with http://localhost:3000/home-page
+//you can see some white space at the bottom of the page. It's probably because the body is screen size, but this page is vertically longer than the screen(depends on window size)
+//we will adjust the shared layout later when we have more pages with real contents.
+/**
+ * The HomePage component renders the home page of the app.
+ *
+ * @returns {JSX.Element} The JSX element representing the home page.
+ */
 export default function HomePage() {
   return (
     <div className="font-normal text-white">
