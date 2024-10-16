3323e11252c53e06806e266ad740c426d81b28ef
maria-pvp <m.usenko@pvp.co.jp>
2024-10-16 13:28:31 +0900

    Separate code into modules, delete unused code, update comments etc.

--- api/controllers/groupsController.js
+++ api/controllers/groupsController.js
@@ -1,57 +1,11 @@
-const { google } = require('googleapis')
-const oauth2Client = require('../models/googleAuth')
-const ServiceAccountKeys = require('../models/ServiceAccountKeys')
-const { getCredentials } = require('../config/googleGroupsConfig')
-const config = require('../config/config')
 const {
   listGroups,
   getGroupByEmail,
   listGroupMembers,
   getAllGroupsLogs,
   getJoinGroupsLogs,
-  getNestedTable,
 } = require('../services/groupsService')
-require('dotenv').config()
-
-/**
- * Decodes the base64-encoded privateKeyData and parses it as JSON.
- *
- * @param {string} privateKeyData - The base64-encoded private key data.
- * @returns {Object} - The decoded and parsed JSON object containing the credentials.
- */
-function decodePrivateKeyData(privateKeyData) {
-  const decodedData = Buffer.from(privateKeyData, 'base64').toString('utf8')
-  return JSON.parse(decodedData)
-}
-
-/**
- * Creates a new JWT client, specifying the user to impersonate and authorizes it.
- *
- * The client is authorized with the scopes required to read the user's groups and
- * the user's audit logs.
- *
- * @param {string} serviceAccountEmail - The email address of the service account.
- * @param {string} privateKey - The private key of the service account.
- * @param {string} userEmail - The email address of the user to impersonate.
- * @returns {Promise<Object>} - A promise that resolves to the authorized client.
- */
-async function getClient(serviceAccountEmail, privateKey, userEmail) {
-  // Create a new JWT client, specifying the user to impersonate
-  const jwtClient = new google.auth.JWT({
-    email: serviceAccountEmail,
-    key: privateKey,
-    scopes: [
-      'https://www.googleapis.com/auth/admin.directory.group',
-      'https://www.googleapis.com/auth/admin.reports.audit.readonly',
-      'https://www.googleapis.com/auth/admin.directory.user.readonly',
-    ],
-    subject: userEmail, // Impersonating this user
-  })
-
-  // Authorize the client
-  await jwtClient.authorize()
-  return jwtClient
-}
+const { getNestedTable } = require('../services/nestedGroupsService')
 
 /**
  * Retrieves the list of all groups in the organization.
@@ -241,6 +195,18 @@ exports.getGroupJoinedActivity = async (req, res) => {
   }
 }
 
+/**
+ * Retrieves a list of nested groups that the group or user with the given email address is a member of.
+ *
+ * This function takes the `userEmail`, `projectId`, `serviceAccountEmail`, `serviceAccountPrivateKey`, and `queryEmail` from the request body.
+ * It uses these values to authorize a JWT client, which it then uses to make a request to the Google Admin Directory API
+ * to list all nested groups that the group with the given email address is a member of.
+ *
+ * @param {Object} req - The request object containing the `userEmail`, `projectId`, `serviceAccountEmail`, `serviceAccountPrivateKey`, and `queryEmail` in the request body.
+ * @param {Object} res - The response object used to return the list of nested groups or an error message.
+ * @returns {Promise<void>} - Responds with the list of nested groups or an error message.
+ * @throws {Error} - Throws an error if the service account key is not found or if there is an issue with the API call.
+ */
 exports.getNestedMembership = async (req, res) => {
   let { userEmail, projectId, serviceAccountEmail, serviceAccountPrivateKey, queryEmail } = req.body
 
