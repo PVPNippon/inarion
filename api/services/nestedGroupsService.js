c5b5f66fef3429288eae5294e1503a31133e2957
maria-pvp <m.usenko@pvp.co.jp>
2024-10-22 10:27:43 +0900

    Refactor nested groups code wave three plus docstrings/comments

--- api/services/nestedGroupsService.js
+++ api/services/nestedGroupsService.js
@@ -90,18 +90,26 @@ function getJoinedTime(allActivities, memberId, groupId) {
   return joinedTime
 }
 
-//WARNING:
-//the logic below is neither optimized nor checked properly.
-//Don't look down here for the sake of your sanity.
+/**
+ * Given an array of group objects, fetches their members and returns
+ * an array of arrays where each subarray contains the members of the
+ * corresponding group. The `includeDerivedMembership` parameter is set
+ * to `true` when calling `listGroupMembers`.
+ * @param {Array<Object>} groups - An array of group objects.
+ * @returns {Promise<Array<Array<Object>>>} - A promise that resolves to an array of arrays of group members.
+ */
 async function getNestedTable(userEmail, projectId, serviceAccountEmail, serviceAccountPrivateKey, queryEmail) {
   /**
-   * Given an array of group objects, fetches their members and returns
-   * an array of arrays where each subarray contains the members of the
-   * corresponding group. The `includeDerivedMembership` parameter is set
-   * to `true` when calling `listGroupMembers`.
+   * Given an array of group objects, fetches all members(direct and indirect) of each group and creates a family array.
+   * The family array contains objects with the group details and the members of each group.
+   *
    * @param {Array<Object>} groups - An array of group objects.
-   * @returns {Promise<Array<Array<Object>>>} - A promise that resolves to an array of arrays of group members.
+   * @param {string} theGroupOrUser - The email address of the group or user to find in the family array.
+   * @returns {Array<Object>} - An array of objects representing the family, each containing the group details and its members.
    */
+  //the reason this function is inside the main function and not at the top of the page
+  //is because I don't want to pass around projectId, serviceAccountEmail, and serviceAccountPrivateKey
+  //I'm passing them around just in case our until it becomes clear they are not needed
   async function getFamilyWithAllMembers(groups, theGroupOrUser) {
     const promises = []
     const family = []
@@ -118,9 +126,11 @@ async function getNestedTable(userEmail, projectId, serviceAccountEmail, service
 
     await Promise.all(promises).then((memberArrays) => {
       memberArrays.forEach((memberArray) => {
+        //if our target group(the star) is among indirect members, add the parent group to the family
         const hasMember = memberArray.filter((member) => member.email === theGroupOrUser)
         if (hasMember.length > 0) {
           const index = memberArrays.indexOf(memberArray)
+          //for each parent group, create a group object containing 2 keys: group and members
           family.push({
             group: groups[index],
             members: memberArray,
@@ -128,14 +138,25 @@ async function getNestedTable(userEmail, projectId, serviceAccountEmail, service
         }
       })
     })
-    return family //returns an array of arrays
+    return family //returns an array of objects containing the group details and all its members(direct+indirect)
+    //for now I keep all the data in, because we might need something else in the future, like group ID or member type etc.
   }
 
+  /**
+   * Given a family array, fetches the direct members of each group in the family and returns an array of arrays.
+   * Each inner array contains the direct members of the corresponding group in the family array.
+   *
+   * @param {Array<Object>} family - An array of objects representing the family, each containing the group details and its members.
+   * @returns {Promise<Array<Array<Object>>>} - A promise that resolves to an array of arrays, where each inner array contains the direct members of the corresponding group in the family array.
+   */
+  //the reason this function is inside the main function and not at the top of the page
+  //is because I don't want to pass around projectId, serviceAccountEmail, and serviceAccountPrivateKey
+  //I'm passing them around just in case our until it becomes clear they are not needed
   async function getDirectMembersArray(family) {
     const promises = []
     const directMemberArray = []
 
-    family.forEach(async (group) => {
+    family.forEach(async (groupObj) => {
       const directMembers = new Promise((resolve, reject) => {
         resolve(
           listGroupMembers(
@@ -143,7 +164,7 @@ async function getNestedTable(userEmail, projectId, serviceAccountEmail, service
             projectId,
             serviceAccountEmail,
             serviceAccountPrivateKey,
-            group.group.email,
+            groupObj.group.email,
             false
           )
         )
@@ -160,60 +181,69 @@ async function getNestedTable(userEmail, projectId, serviceAccountEmail, service
     return directMemberArray
   }
 
+  /**
+   * Given a family of groups, retrieves the membership details of each direct/indirect parents of the target group/user.
+   * For each direct member, determines if their membership is direct or inherited, and provides the timestamp of their membership.
+   *
+   * @param {Object[]} family - A list of groups, each containing a group object and a list of all its members(direct and indirect).
+   * @returns {Object[]} - An array of objects containing membership details for direct/indirect parents of the target group.
+   */
   async function getTable(family) {
     const table = []
+
+    //get an array with group joining logs for the last 6 months
+    //the reason why we need them is because google API does not provide a joined timestamp
+    //so we need to get it ourselves in a roundabout way
+    const allActivities = await getJoinGroupsLogs(userEmail, projectId, serviceAccountEmail, serviceAccountPrivateKey)
+
+    //for each group in the family, fetch all its direct members
+    //the reason why we need to fetch all direct members separately is because
+    //the API method which returns all members(direct and indirect) does not say which member is direct and which is indirect
+    //so we need to find it out by ourselves in a roundabout way
     const directMembers = await getDirectMembersArray(family)
+
+    //for each parent group, create JSON object containing membership details
     directMembers.forEach((directMember) => {
       const index = directMembers.indexOf(directMember)
-      const group = family[index]
+      const groupObj = family[index]
       const obj = {
-        email: group.group.email,
-        timestamp: '',
+        email: groupObj.group.email, //the "group" column of the table
       }
 
-      const targetGroup = directMember.filter((member) => member.email === theGroupOrUser)
-      if (targetGroup.length > 0) {
-        obj.membership = 'Direct'
-        obj.inherited = ''
+      //sift through for each group's direct member array to find out if our target group is among direct members
+      const targetParentGroup = directMember.filter((member) => member.email === theGroupOrUser)
+
+      if (targetParentGroup.length > 0) {
+        obj.membership = 'Direct' //the "membership type" column of the table
+        obj.inherited = '' //"inherited via" column of the table, left empty for direct memberships
+        obj.timestamp = getJoinedTime(allActivities, theGroupOrUser, groupObj.group.email)
       } else {
-        const inheritedVia = getTransitive(family, directMembers, theGroupOrUser, group.group.email) || ''
-        obj.membership = 'Inherited'
-        obj.inherited = inheritedVia
+        const inheritedVia = getTransitive(family, directMembers, theGroupOrUser, groupObj.group.email) || ''
+        obj.membership = 'Inherited' //the "membership type" column of the table
+        obj.inherited = inheritedVia //"inherited via" column of the table
+        obj.timestamp = '' //"timestamp" column of the table, left empty for inherited memberships (for now)
       }
       table.push(obj)
     })
     return table
   }
 
-  async function updateTable(table) {
-    const allActivities = await getJoinGroupsLogs(userEmail, projectId, serviceAccountEmail, serviceAccountPrivateKey)
-    const updatedTable = table
-
-    updatedTable.forEach((member) => {
-      let parentEmail
-      if (member.membership === 'Inherited') {
-        member.timestamp = ''
-      } else if (member.membership === 'Direct') {
-        parentEmail = member.email
-        member.timestamp = getJoinedTime(allActivities, theGroupOrUser, parentEmail)
-      }
-    })
-
-    return updatedTable
-  }
-
   const theGroupOrUser = queryEmail
 
   try {
+    //get a list of all groups in customer organization
     let groups = await listGroups(userEmail, projectId, serviceAccountEmail, serviceAccountPrivateKey)
+
+    //leave out groups with no members to reduct amoung of API calls
     groups = groups.filter((group) => group.directMembersCount > 0)
 
+    //get an array of parent group objects
     const family = await getFamilyWithAllMembers(groups, theGroupOrUser)
 
-    //TO DO need to refactor logic to return the table at once(and not only that)
+    //get an array with membership details and timestamp for each parent/grandparent group of the target group/user
     const table = await getTable(family)
-    const updatedTable = await updateTable(table)
-    return updatedTable
+
+    return table
   } catch (error) {
     console.log(error)
   }
