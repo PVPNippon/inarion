f0b68672142a10da08c6e117646fbc5a64f5f501
maria-pvp <m.usenko@pvp.co.jp>
2025-01-24 13:22:23 +0900

    Bypass encryption for the groups hierarchy graph(temporarily)

--- next-ui/app/ui/components/VisualizeHierarchy.jsx
+++ next-ui/app/ui/components/VisualizeHierarchy.jsx
@@ -1,6 +1,5 @@
 'use client'
-import React, { useState, useEffect, useContext } from 'react'
-import { LoggedInUserContext } from '../contexts/LoggedInUserContext'
+import React, { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { apiClient } from '@/utils/apiClient'
@@ -25,7 +24,7 @@ import { apiClient } from '@/utils/apiClient'
 function VisualizeHierarchy() {
   //temporary component for dev purposes
   //used to test the groups hierarchy visualiser backend code
-  const { email } = useContext(LoggedInUserContext)
+
   const [inputValue, setInputValue] = useState('')
   const [groupList, setGroupList] = useState({
     nodes: [],
@@ -33,6 +32,7 @@ function VisualizeHierarchy() {
   })
   const [clickCount, setClickCount] = useState(0)
   const [error, setError] = useState(null)
+  let email
 
   useEffect(() => {
     /**
@@ -52,21 +52,42 @@ function VisualizeHierarchy() {
           return
         }
 
-        const response = await apiClient(
-          '/api/groups/get-hierarchy', // Endpoint path relative to API_BASE_URL
-          'POST', // HTTP method
+        email = window.localStorage.getItem('email')
+        console.log('email:', email)
+
+        const token = localStorage.getItem('jwtToken')
+        console.log('TOKEN', token)
+        //while FE is broken, falling back to the good old fetch(beware of cache though)
+        const response = await fetch(
+          `http://localhost:4000/api/groups/target/${inputValue}/hierarchy?userEmail=${email}`,
           {
-            userEmail: email,
-            queryEmail: inputValue,
-          },
-          {}, // Additional headers, if any
-          true // withCredentials flag
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            method: 'GET',
+            cache: 'no-store', //this disables cache
+          }
         )
+
+        //disabled apiClient because the logic for get requests is not ready yet
+        // const response = await apiClient(
+        //   '/api/groups/get-hierarchy', // Endpoint path relative to API_BASE_URL
+        //   'POST', // HTTP method
+        //   {
+        //     userEmail: email,
+        //     queryEmail: inputValue,
+        //   },
+        //   {}, // Additional headers, if any
+        //   true // withCredentials flag
+        // )
         // if emtpy data is returned, set error 'No memberships found', otherwise set groupList
         if (response) {
-          //I parse the response data to JSON because I need to do some manipulation with it before storing it in local storage
-          //if in the future there is no need to manipulate the data anymore, you can omit this step and just store it as a string in the local storage
-          const responseData = JSON.parse(response)
+          console.log('response:', response)
+          // //I parse the response data to JSON because I need to do some manipulation with it before storing it in local storage
+          // //if in the future there is no need to manipulate the data anymore, you can omit this step and just store it as a string in the local storage
+          // const responseData = JSON.parse(response)
+          const responseData = await response.json()
+          console.log('responseData:', responseData)
 
           if (responseData.length === 0) {
             setError({ message: 'No memberships found. Please check if the email address is correct and try again.' })
