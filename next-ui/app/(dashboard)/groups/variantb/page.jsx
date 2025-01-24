c267a5f42608d1e0dca9a87c83dbe3bceb0dff1e
maria-pvp <m.usenko@pvp.co.jp>
2025-01-24 14:20:18 +0900

    Move nested groups table to the hierarchy page and create a placeholder page for variant b

---
+++ next-ui/app/(dashboard)/groups/variantb/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+import React, { useState, useEffect, useContext } from 'react'
+function NestedGroupsListerVariantB() {
+  return (
+    <div>
+      <p>This is a placeholder for the nested group memberships page Variant B</p>
+    </div>
+  )
+}
+
+export default NestedGroupsListerVariantB
