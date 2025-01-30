a32b63b356d120632df114699c6336d7543657b2
maria-pvp <m.usenko@pvp.co.jp>
2025-01-30 15:50:39 +0900

    VariantB: remove logic which hides the search bar

--- next-ui/app/(dashboard)/groups/groups-styles.js
+++ next-ui/app/(dashboard)/groups/groups-styles.js
@@ -3,6 +3,7 @@ export const groupsStyles = {
   buttonPadding: 'py-2 px-6 gap-x-2.5', //top-bottom 8px left-right 24px gap 10px
   buttonPaddingWide: 'py-2 px-8 gap-x-2.5', //top-bottom 8px left-right 32px gap 10px
   searchBarWidth: 'max-w-[627px]',
+  searchBarWidthVariantB: 'sm:w-[627px] min-w-[200px]',
   roundBorder: 'border border-input rounded-lg', //radius 8px
   thinShadow: 'shadow-[0_1px_5px_0_rgba(239,80,57,0.08)]',
   tableRowPadding: 'py-3', //top-bottom 12px
