6cbb33258c0a05c2ff9ae7c2aa8afc8668f9fec3
Srihitha <s.ravi@pvp.co.jp>
2025-03-28 13:56:21 +0900

    Spinner and some css are changed

--- next-ui/src/app/[locale]/static-page/page.jsx
+++ next-ui/src/app/[locale]/static-page/page.jsx
@@ -1213,7 +1213,7 @@ export default function StaticPage() {
         <div className=" flex flex-col items-center justify-center space-y-2">
           <div className="spinner-wrapper">
             <svg className="spinner" viewBox="0 0 50 50">
-              <circle className="path" cx="25" cy="25" r="20" fill="none" stroke="#ef5039" stroke-width="4" />
+              <circle className="path" cx="25" cy="25" r="20" fill="none" stroke="#ef5039" strokeWidth="4" />
             </svg>
           </div>
 
